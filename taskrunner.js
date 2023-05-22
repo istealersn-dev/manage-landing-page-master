7c21fca656557844372e696e1e68ed5cc19c3acb
istealersn.dev <istealersn.dev@gmail.com>
2023-05-23 00:16:13 +0530

    fixed Sass setup issues and resolved ESLint errors

--- taskrunner.js
+++ taskrunner.js
@@ -9,53 +9,53 @@ const browsersync = require('browser-sync').create();
 
 // Sass compilation
 function sassCompile() {
-    return src("src/sass/main.scss", { sourcemaps: true})
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(dest('dist/css', { sourcemaps: 'map'}));
+	return src('src/sass/main.scss', { sourcemaps: true})
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('dist/css', { sourcemaps: 'map'}));
 }
 
 // Javascript compilation
 function jsCompile() {
-    return src('src/js/main.js', { sourcemaps: true })
-    .pipe(babel({ presets: ['@babel/preset-env'] }))
-    .pipe(terser())
-    .pipe(dest('dist/js', { sourcemaps: '.'}))
+	return src('src/js/main.js', { sourcemaps: true })
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.pipe(terser())
+		.pipe(dest('dist/js', { sourcemaps: '.'}));
 }
 
 // Browser sync setup
 function browserSyncservice(callback) {
-    browsersync.init({
-        server: {
-            baseDir: '.',
-        },
-        notify: {
-            styles: {
-                top: 'auto',
-                bottom: '0',
-            },
-        },
-    });
-    callback();
+	browsersync.init({
+		server: {
+			baseDir: '.',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+	callback();
 }
 
 // Browser sync reload
 function browserSyncReload(callback) {
-    browsersync.reload();
-    callback();
+	browsersync.reload();
+	callback();
 }
 
 // Watch for HTML change to sync the browser
 function watchHtml() {
-    watch('*.html', browserSyncReload);
-    watch(
-        ['src/sass/**/*.scss'],
-        series(sassCompile, browserSyncReload)
-    );
-    watch(
-        ['src/js/*.js'],
-        series(jsCompile, browserSyncReload)
-    );
+	watch('*.html', browserSyncReload);
+	watch(
+		['src/sass/**/*.scss'],
+		series(sassCompile, browserSyncReload)
+	);
+	watch(
+		['src/js/*.js'],
+		series(jsCompile, browserSyncReload)
+	);
 }
 
 // Default
